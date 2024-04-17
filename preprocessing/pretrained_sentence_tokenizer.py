0d4725edbf2690360af32912543a0c15239bb72c
wilson melo <bakudas@gmail.com>
2024-04-17 19:29:02 -0300

    pretrained sentence tokenizer

---
+++ preprocessing/pretrained_sentence_tokenizer.py
@@ -0,0 +1,30 @@
+#%%
+import nltk
+from nltk.corpus import europarl_raw
+nltk.download('europarl_raw')
+german_text = europarl_raw.german.raw(fileids='ep-00-01-17.de')
+
+# %%
+# Total characters in the corpus
+print(len(german_text))
+# First 100 chars in the corpus
+print(german_text[:100])
+
+# %%
+# Default sentece tokenizer
+default_st = nltk.sent_tokenize
+german_default_st = default_st(text=german_text, language='german')
+
+# %%
+# Load the german tokenizer
+german_tokenizer = nltk.data.load(resource_url='tokenizers/punkt/german.pickle')
+german_sentences = german_tokenizer.tokenize(german_text)
+
+# %%
+print(type(german_tokenizer))
+print(german_default_st == german_sentences)
+
+# %%
+import numpy as np
+# Print the first 5 sentences
+print(np.array(german_sentences[:5]))
